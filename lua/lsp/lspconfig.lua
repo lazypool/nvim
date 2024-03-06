6695e626c9420127df1e5930968dd21065c1791f
lazypool <3416382833@qq.com>
2024-03-06 13:19:42 +0800

    equip with formatter and linter

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -1,26 +1,26 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = { "hrsh7th/cmp-nvim-lsp" },
-  config = function()
-    local lspconfig = require "lspconfig"
-    local capabilities = require "cmp_nvim_lsp".default_capabilities()
+	"neovim/nvim-lspconfig",
+	dependencies = { "hrsh7th/cmp-nvim-lsp" },
+	config = function()
+		local lspconfig = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    lspconfig.pyright.setup {
-      capabilities = capabilities,
-      settings = {
-        pyright = {
-          reportGeneralTypeIssues = false,
-          reportUnknownArgumentType = false,
-        }
-      }
-    }
-
-		lspconfig.clangd.setup {
+		lspconfig.pyright.setup({
 			capabilities = capabilities,
-		}
+			settings = {
+				pyright = {
+					reportGeneralTypeIssues = false,
+					reportUnknownArgumentType = false,
+				},
+			},
+		})
 
-		lspconfig.tsserver.setup {
+		lspconfig.clangd.setup({
 			capabilities = capabilities,
-		}
-  end
+		})
+
+		lspconfig.tsserver.setup({
+			capabilities = capabilities,
+		})
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,37 +1,49 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
 		"windwp/nvim-ts-autotag",
 	},
-  config = function()
-    require "nvim-treesitter.configs".setup {
-      -- 选择需要高亮的语法
-      ensure_installed = {
-        "bash", "c", "cpp", "css", "go",
-        "html", "java", "latex", "lua",
-				"markdown", "python", "vim",
-				"typescript", "tsx", "xml", "query"
-      },
-    
-      -- 启用高亮
-      highlight = {
-        enable = true,
-      },
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			-- 选择需要高亮的语法
+			ensure_installed = {
+				"bash",
+				"c",
+				"cpp",
+				"css",
+				"go",
+				"html",
+				"java",
+				"latex",
+				"lua",
+				"markdown",
+				"python",
+				"vim",
+				"typescript",
+				"tsx",
+				"xml",
+				"query",
+			},
 
-      -- 禁用彩色括号，原作者不再维护
-      rainbow = {
-        enable = false,
-      },
+			-- 启用高亮
+			highlight = {
+				enable = true,
+			},
 
-      -- 启用缩进
-      indent = {
-        enable = true,
-      },
+			-- 禁用彩色括号，原作者不再维护
+			rainbow = {
+				enable = false,
+			},
+
+			-- 启用缩进
+			indent = {
+				enable = true,
+			},
 
 			-- 启用自动标签补齐
 			autotag = {
 				enable = true,
-			}
-    }
-  end
+			},
+		})
+	end,
 }
