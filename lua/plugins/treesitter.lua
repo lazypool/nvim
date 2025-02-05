7deb9d4722b3da5c5a628e196cf688acaec503e3
lazypool <3416382833@qq.com>
2025-02-05 20:14:01 +0800

    v1.0.4 - 修正 treesitter 在使用过程中的 BUG

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,50 +1,19 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-	},
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- 选择需要高亮的语法
-			ensure_installed = {
-				"bash",
-				"c",
-				"cpp",
-				"css",
-				"go",
-				"html",
-				"java",
-				"latex",
-				"lua",
-				"markdown",
-				"python",
-				"vim",
-				"typescript",
-				"tsx",
-				"xml",
-				"javascript",
-			},
-
-			-- 启用高亮
-			highlight = {
-				enable = true,
-			},
-
-			-- 禁用彩色括号，原作者不再维护
-			rainbow = {
-				enable = false,
-			},
-
-			-- 启用缩进
-			indent = {
-				enable = true,
-			},
-
-			-- 禁用自动标签补齐
-			-- 自 2024 年 5 月 28 日该方法已被原作者弃用
-			autotag = {
-				enable = false,
-			},
-		})
-	end,
-}
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function () 
+		  require("nvim-treesitter.configs").setup({
+		    	ensure_installed = { 
+						"c", "lua", "vim", "vimdoc", "query",
+						"elixir", "heex", "javascript", "html",
+						"markdown", "latex"
+					},
+          sync_install = false,
+          highlight = {
+						enable = true,
+						disable = { "latex" }
+					},
+          indent = { enable = true },  
+        })
+    end
+ }
