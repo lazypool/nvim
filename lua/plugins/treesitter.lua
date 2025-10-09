63ad1ee8c613c3d3cc0cdbfccbb18fb1575efb43
lazypool <lazypool@proton.me>
2025-10-10 00:23:39 +0800

    v1.0.13 - Updating: 2025-10-10

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,30 @@
 return {
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function ()
-		  require("nvim-treesitter.configs").setup({
-					ensure_installed = {
-						"c", "lua", "vim", "vimdoc", "query",
-						"elixir", "heex", "javascript", "html",
-						"markdown",
-					},
-          sync_install = false,
-          highlight = {
-						enable = true,
-						disable = { "latex" }
-					},
-          indent = { enable = true },
-        })
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function ()
+	  require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"elixir",
+				"heex",
+				"javascript",
+				"html",
+				"markdown",
+			},
+      sync_install = false,
+      highlight = {
+				enable = true,
+				disable = {
+					"latex",
+				}
+			},
+      indent = {
+				enable = true
+			},
+    })
+  end
  }
